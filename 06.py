2a619b038faea61a5ee6423a5f911dd965d22b22
MeiaSuja53 <nickalves2313@gmail.com>
2025-06-27 22:36:02 -0300

    básico da aritmética

---
+++ 06.py
@@ -0,0 +1,8 @@
+# Exercicio 06 = Faça um programa que leia um numero e mostre o seu sucessor e antecessor.
+
+num = int(input('Digite um numero: '))
+ant = num - 1
+suc = num + 1
+
+print(f'O antecessor de {num} é: {ant}.')
+print(f'O sucessor de {num} é: {suc}.')
