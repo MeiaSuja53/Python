6ac4f941235ecf0eede9266200bcfe792bad645b
MeiaSuja53 <nickalves2313@gmail.com>
2025-06-29 22:12:05 -0300

    básico da aritmética

--- 07.py
+++ 07.py
@@ -1,1 +1,10 @@
 #Exercício 07: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+num = int(input('Digite um numero: '))
+do = num * 2
+tri = num * 3
+qua = num ** (1/2)
+
+print(num)
+print(f'O dobro de: {num} vale {do}.')
+print(f'O triplo de: {num} vale {tri}.')
+print(f'A raiz quadrada de: {num} vale {qua}.')
