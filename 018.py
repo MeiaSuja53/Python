7a3b3de2d278e525b9d80214973d4d78831df735
MeiaSuja53 <nickalves2313@gmail.com>
2025-07-07 11:59:19 -0300

    usando biblioteca (math)

---
+++ 018.py
@@ -0,0 +1,10 @@
+# Exercício 18: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+import math
+angulo = float(input('Digite o ângulo que você deseja: '))
+seno = math.sin(math.radians(angulo))
+cos = math.cos(math.radians(angulo))
+tan = math.tan(math.radians(angulo))
+
+print('O ângulo de {:.2f} tem o SENO de {:.2f}'.format(angulo, seno))
+print('O ângulo de {:.2f} tem o COSSENO de {:.2f}'.format(angulo, cos))
+print('O ângulo de {:.2f} tem a TANGENTE de {:.2f}'.format(angulo, tan))
